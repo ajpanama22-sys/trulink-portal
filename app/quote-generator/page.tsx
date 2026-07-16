9f6a0883883af2982b862b7a607e53d43dbf44ac
ajpanama22-sys <ajpanama22@gmail.com>
2026-07-16 02:30:43 -0500

    Delete app/quote-generator/page.tsx

--- app/quote-generator/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function QuoteGeneratorPage() {
-  return (
-    <div style={{ padding: "2rem", backgroundColor: "#000", color: "#FFD700" }}>
-      <h1>Quote Generator</h1>
-      <p>Bienvenido al portal B2B de Trulink Fiber.</p>
-    </div>
-  );
-}
