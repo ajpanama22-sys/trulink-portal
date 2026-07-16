4564fd42d5c2541ded22e08a2767973a19d45010
ajpanama22-sys <ajpanama22@gmail.com>
2026-07-16 01:02:46 -0500

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  experimental: {
-    appDir: false,   // 👈 esto desactiva el App Router
-  },
-};
-
-export default nextConfig;
