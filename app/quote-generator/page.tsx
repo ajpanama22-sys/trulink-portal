e076833738222b987dfa0014de1d423c1ce1f6d6
Fred Jurado <ajpanama22@gmail.com>
2026-07-17 11:40:35 -0500

    Eliminar carpeta app

--- app/quote-generator/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function QuoteGeneratorPage() {
-  return (
-    <div style={{ padding: "2rem", backgroundColor: "#000", color: "#FFD700" }}>
-      <h1>Generador de Cotizaciones</h1>
-      <p>Bienvenido al módulo B2B de Trulink Fiber.</p>
-    </div>
-  );
-}
-
