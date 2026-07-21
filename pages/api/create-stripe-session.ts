fa8edc03fc48982f89cd67bcdf1eb3854b9336dc
Fred Jurado <ajpanama22@gmail.com>
2026-07-20 21:39:43 -0500

    Corrige version de API de Stripe

--- pages/api/create-stripe-session.ts
+++ pages/api/create-stripe-session.ts
@@ -1,9 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2025-02-28.acacia' as any,
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '');
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
