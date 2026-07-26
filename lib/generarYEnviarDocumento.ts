ee8336aa35bd81a2f142ef55185ac18bbf0041d8
Fred Jurado <ajpanama22@gmail.com>
2026-07-26 15:25:08 -0500

    update

--- lib/generarYEnviarDocumento.ts
+++ lib/generarYEnviarDocumento.ts
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 import PDFDocument from "pdfkit";
-import fetch from "node-fetch";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
