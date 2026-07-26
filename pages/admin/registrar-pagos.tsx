2adac673681d792c5977dca31fd5c6e9c256be9a
Fred Jurado <ajpanama22@gmail.com>
2026-07-26 15:28:20 -0500

    update

--- pages/admin/registrar-pagos.tsx
+++ pages/admin/registrar-pagos.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Sidebar from "../../components/Sidebar";
+import Sidebar from "@/components/Sidebar";
 
 export default function RegistrarPagoVisual() {
   const [cargando, setCargando] = useState(false);
